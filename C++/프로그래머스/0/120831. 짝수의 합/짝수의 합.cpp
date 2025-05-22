8874787b35be8bbc5c6aeeee544bfafd736a759c
KimHyeongmo <93339488+KimHyeongmo@users.noreply.github.com>
2025-05-23 01:39:13 +0900

    [level 0] Title: 짝수의 합, Time: 0.01 ms, Memory: 3.59 MB -BaekjoonHub

--- C++/프로그래머스/0/120831. 짝수의 합/짝수의 합.cpp
+++ C++/프로그래머스/0/120831. 짝수의 합/짝수의 합.cpp
@@ -4,13 +4,8 @@
 using namespace std;
 
 int solution(int n) {
-    int answer = 0;
-    for(int i = 1; i<=n; i++)
-    {
-        if(i%2 == 0)
-        {
-            answer += i;
-        }
-    }
+    int answer;
+    n /= 2;
+    answer = n * (1 + n);
     return answer;
 }
