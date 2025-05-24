1c2305551dd48e860664e08814566d2a211fdb05
KimHyeongmo <93339488+KimHyeongmo@users.noreply.github.com>
2025-05-24 23:37:52 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.20 ms, Memory: 4.15 MB -BaekjoonHub

--- C++/프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.cpp
+++ C++/프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.cpp
@@ -3,47 +3,11 @@
 
 using namespace std;
 
-bool DivideMoc(int number, int division, int& start)
-{
-    if((number / division) >= division)
-    {
-        start = division;
-        return true;
-    }
-    return false;
-}
-
-void CalType(int number, int& start)
-{
-    if(DivideMoc(number, 1000, start))
-        return;
-    if(DivideMoc(number, 500, start))
-        return;
-    if(DivideMoc(number, 250, start))
-        return;
-    if(DivideMoc(number, 100, start))
-        return;
-    if(DivideMoc(number, 50, start))
-        return;
-    if(DivideMoc(number, 10, start))
-        return;
-
-    start = 1;
-    return;
-}
-
 int solution(int n) {
-    
-    int start = 1;
-    CalType(n, start);
-    for(int i = start; i <= 1000000; i++)
-    {
-        if(n / i == i && n % i == 0)
-        {
-            return 1;
-        }
+    int answer = 0;
+    for(int i = 0; i < n; i++){
+        if(i*i == n) return 1;
     }
-    
+
     return 2;
-    
 }
