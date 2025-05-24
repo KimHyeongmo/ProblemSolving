e2202729335e93947792b20d999151a63f964113
KimHyeongmo <93339488+KimHyeongmo@users.noreply.github.com>
2025-05-25 00:49:13 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, int n) {
+    string answer = "";
+    int index = my_string.length();
+    string tempt(n, ' ');
+    
+    for(int i = 0; i<index; i++)
+    {
+        for(int j = 0; j<n; j++)
+        {
+            tempt[j] = my_string[i];
+        }
+        answer.append(tempt);
+    }
+    
+    return answer;
+}
