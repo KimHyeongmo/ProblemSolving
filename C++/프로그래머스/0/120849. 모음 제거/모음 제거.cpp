f9d33e2cb3bcfb13502973db225b7bf5ef97aac0
KimHyeongmo <93339488+KimHyeongmo@users.noreply.github.com>
2025-05-28 00:06:04 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 3.66 MB -BaekjoonHub

--- C++/프로그래머스/0/120849. 모음 제거/모음 제거.cpp
+++ C++/프로그래머스/0/120849. 모음 제거/모음 제거.cpp
@@ -7,18 +7,17 @@ string solution(string my_string) {
     string answer = "";
     for(auto i = my_string.begin(); i != my_string.end(); i++)
     {
-        if(*i == 'a')
-            continue;
-        else if(*i == 'e')
-            continue;
-        else if(*i == 'i')
-            continue;
-        else if(*i == 'o')
-            continue;
-        else if(*i == 'u')
-            continue;
-        
-        answer.push_back(*i);
+        switch(*i)
+        {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                break;
+            default :     
+                answer.push_back(*i);
+        }
     }
     return answer;
 }
